493dc7927c8dd6b00f46e4193dfc370fa3612979
DanielaCordova <dancordo@ucm.es>
2021-03-17 20:05:30 +0100

    Practica2 - AlgoritmoRompeEmpate

---
+++ Daniela/practica2C/src/practica2C/VariableSicronizacion.java
@@ -0,0 +1,54 @@
+package practica2C;
+
+public class VariableSicronizacion {
+
+private volatile  boolean  ini1, ini2;
+private volatile  int last;
+
+	public VariableSicronizacion(boolean ini1, boolean ini2, int last) {
+		this.ini1=ini1;
+		this.ini2=ini2;
+		this.last=last;
+	}
+	
+	private void setIni1(boolean v) {
+		this.ini1=v;
+	}
+	private void setIni2(boolean v) {
+		this.ini2=v;
+	}
+	
+	private void setLast(int v) {
+		this.last=v;
+	}
+	
+	private boolean getIni1() {
+		return this.ini1;
+	}
+	private boolean getIni2() {
+		return this.ini2;
+	}
+	
+	private int getLast() {
+		return this.last;
+	}
+	
+	public void lock1() {
+		setIni1(true);
+		setLast(1);
+		while(getIni2() && getLast()==1);
+	}
+	
+	public void unlock1() {
+		setIni1(false);
+	}
+	public void lock2() {
+		setIni2(true);
+		setLast(2);
+		while(getIni1() && getLast()==2);
+	}
+	
+	public void unlock2() {
+		setIni2(false);
+	}
+}
